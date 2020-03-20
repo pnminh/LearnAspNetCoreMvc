f98b1018a96933fb04efa858d1adba72dc5c91cc
pnminh <pnminh232@gmail.com>
2020-03-20 18:27:40 -0500

    ids in different ways

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,22 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
-namespace LearnAspNetCoreMvc.Controllers{
-    public class HomeController:Controller{
-        public String index(){
+namespace LearnAspNetCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String index()
+        {
             return "Hello from Home Controller";
+        }
+        public IActionResult echo(string id)
+        {
+            return new ContentResult { Content = id };
+        }
+        public IActionResult echoWithDefault(string id = "default"){
+            return new ContentResult {Content = id};
+        }
+        public IActionResult echoWithNullable(int? id){
+            return new ContentResult {Content = id==null?"NULL":id.ToString()};
         }
     }
 }
