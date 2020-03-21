f8568fbb987fdc9ef9fa53f50e1bad9570f6f3a8
pnminh <pnminh232@gmail.com>
2020-03-20 22:09:20 -0500

    use layout

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -8,7 +8,7 @@ namespace LearnAspNetCoreMvc.Controllers
         [HttpGet]
         public IActionResult index()
         {
-            return new ContentResult { Content = "From BlogController with love" };
+            return View();
         }
         [Route("long_year/{year:min(1980)}/{month:range(1,12)}/{date:range(1,31)}")]
         public IActionResult LongYear(int year, int month, int date)
