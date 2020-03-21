847b954ad0dff60efc9050a6f45fdea3cfb3faa8
pnminh <pnminh232@gmail.com>
2020-03-20 21:04:15 -0500

    route details

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace LearnAspNetCoreMvc.Controllers{
-    public class BlogController:Controller{
-        public IActionResult index(){
-            return new ContentResult{Content="From BlogController with love"};
+namespace LearnAspNetCoreMvc.Controllers
+{
+    [Route("blogs")]
+    public class BlogController : Controller
+    {
+        [HttpGet]
+        public IActionResult index()
+        {
+            return new ContentResult { Content = "From BlogController with love" };
         }
+        [Route("long_year/{year:min(1980)}/{month:range(1,12)}/{date:range(1,31)}")]
+        public IActionResult LongYear(int year, int month, int date)
+        {
+            return new ContentResult { Content = string.Format("Year: {0}, Month: {1}, Day: {2}", year, month, date) };
+
+        }
+
     }
 }
