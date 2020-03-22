b9c91d38d05c0d295b36133bcc93abba824ae9b8
pnminh <pnminh232@gmail.com>
2020-03-21 22:04:59 -0500

    use partial views

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -10,7 +10,20 @@ namespace LearnAspNetCoreMvc.Controllers
         [HttpGet("")]
         public IActionResult Index()
         {
-            return View();
+            return View(new[]{
+                new Post {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "John Doe",
+                    Body = "This is a great blog post, don't you think?"
+                },
+                new Post {
+                    Title = "My 2nd blog post",
+                    Posted = DateTime.Now,
+                    Author = "John Doe",
+                    Body = "This is another great blog post, don't you think?"
+                },
+            });
         }
         [HttpGet("{year:min(1900)}/{month:range(1,12)}/{key}")]
         public IActionResult GetPost(int year, int month, string key)
