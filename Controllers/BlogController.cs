decb92c2cd3c19546e0ffc01fdff01dfbb84c7bc
pnminh <pnminh232@gmail.com>
2020-03-21 21:37:13 -0500

    use model

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -13,12 +13,15 @@ namespace LearnAspNetCoreMvc.Controllers
             return View();
         }
         [HttpGet("{year:min(1900)}/{month:range(1,12)}/{key}")]
-        public IActionResult GetPost(int year, int month, string key){
-            ViewBag.Title = "My blog post";
-            ViewBag.Posted = DateTime.Now;
-            ViewBag.Author = "John Doe";
-            ViewBag.Body = "This is a great blog post, don't you think?";
-            return View("post");
+        public IActionResult GetPost(int year, int month, string key)
+        {
+            return View("post", new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "John Doe",
+                Body = "This is a great blog post, don't you think?"
+            });
         }
 
     }
---
+++ Models/Post.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LearnAspNetCoreMvc.Models
+{
+    public class Post
+    {
+        public string Title{get;set;}
+        public string Author{get;set;}
+        public string Body{get;set;}
+        public DateTime Posted{get;set;}
+    }
+}
