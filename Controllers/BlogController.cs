71c13bebd9aaa3f6e98ba32e549d7dfee2929047
pnminh <pnminh232@gmail.com>
2020-03-22 23:53:49 -0500

    use partial view with ajax

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -4,44 +4,59 @@ using LearnAspNetCoreMvc.Models;
 using LearnAspNetCoreMvc.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LearnAspNetCoreMvc.Controllers
-{
-    [Route("blog")]
-    public class BlogController : Controller
-    {
+namespace LearnAspNetCoreMvc.Controllers {
+    [Route ("blog")]
+    public class BlogController : Controller {
         private readonly BlogDataContext _blogDbContext;
-        public BlogController(BlogDataContext blogDataContext)
-        {
+        public BlogController (BlogDataContext blogDataContext) {
             this._blogDbContext = blogDataContext;
         }
-        [HttpGet("")]
-        public IActionResult Index(int page = 0)
-        {
-            var posts = _blogDbContext.Posts.OrderByDescending(x => x.Posted).Take(5).ToArray();
-            return View(posts);
+
+        [HttpGet ("")]
+        public IActionResult Index (int page = 0) {
+            var pageSize = 1;
+            var totalPosts = _blogDbContext.Posts.Count ();
+            var totalPages = totalPosts / pageSize;
+            var previousPage = page - 1;
+            var nextPage = page + 1;
+            ViewBag.PreviousPage = previousPage;
+            ViewBag.HasPreviousPage = previousPage >= 0;
+            ViewBag.NextPage = nextPage;
+            ViewBag.HasNextPage = nextPage < totalPages;
+            var posts = _blogDbContext.Posts
+                .OrderByDescending (x => x.Posted)
+                .Skip (pageSize * page)
+                .Take (pageSize)
+                .ToArray ();
+
+            // AJAX call
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest") return PartialView (posts);//not render Viewstart
+            return View (posts);
         }
-        [HttpGet("{year:min(1900)}/{month:range(1,12)}/{key}")]
-        public IActionResult GetPost(int year, int month, string key)
-        {
-            var post = _blogDbContext.Posts.FirstOrDefault(x => x.Key == key);
-            return View("post", post);
+
+        [HttpGet ("{year:min(1900)}/{month:range(1,12)}/{key}")]
+        public IActionResult GetPost (int year, int month, string key) {
+            var post = _blogDbContext.Posts.FirstOrDefault (x => x.Key == key);
+            return View ("post", post);
         }
-        [HttpGet("create")]
-        public IActionResult Create(){
-            return View();
+
+        [HttpGet ("create")]
+        public IActionResult Create () {
+            return View ();
         }
-        [HttpPost("create")]
-        public IActionResult Create(Post post){
-            if(!ModelState.IsValid)return View();
+
+        [HttpPost ("create")]
+        public IActionResult Create (Post post) {
+            if (!ModelState.IsValid) return View ();
             post.Author = User.Identity.Name;
             post.Posted = DateTime.Now;
-            _blogDbContext.Posts.Add(post);
-            _blogDbContext.SaveChanges();
+            _blogDbContext.Posts.Add (post);
+            _blogDbContext.SaveChanges ();
 
-            return RedirectToAction("GetPost","Blog", new {
+            return RedirectToAction ("GetPost", "Blog", new {
                 year = post.Posted.Year,
-                month = post.Posted.Month,
-                key = post.Key
+                    month = post.Posted.Month,
+                    key = post.Key
             });
         }
     }
---
+++ wwwroot/scripts/site.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#mainContent').on('click','.pager a', function(){
+        var url = $(this).attr('href');
+        $('#mainContent').load(url);
+        return false;
+    })
+});
