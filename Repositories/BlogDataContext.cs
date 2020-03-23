896637c7b45c28045f2c04355ea3c8c4457412ba
pnminh <pnminh232@gmail.com>
2020-03-23 12:37:48 -0500

    use Comment Db

--- Repositories/BlogDataContext.cs
+++ Repositories/BlogDataContext.cs
@@ -6,6 +6,7 @@ namespace LearnAspNetCoreMvc.Repositories
     public class BlogDataContext:DbContext
     {
         public DbSet<Post> Posts {get;set;}
+        public DbSet<Comment> Comments{get;set;}
         public BlogDataContext(DbContextOptions<BlogDataContext> options):base(options){
             Database.EnsureCreated();
         }
--- apis/CommentsController.cs
+++ apis/CommentsController.cs
@@ -1,22 +1,38 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using LearnAspNetCoreMvc.Models;
+using LearnAspNetCoreMvc.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LearnAspNetCoreMvc.apis
 {
-    [Route("api/[controller]")]
+    [Route("api/posts/{postKey}/[controller]")]
     public class CommentsController:Controller
     {
+        private readonly BlogDataContext _dbContext;
+        public CommentsController(BlogDataContext dbContext){
+            this._dbContext = dbContext;
+        }
         [HttpGet]
-        public IEnumerable<string> Get(){
-            return new string[]{"value1", "value2"};
+        public List<Comment> Get(string postKey){
+            return this._dbContext.Comments.Where(x => x.Post.Key == postKey).ToList();
         }
 
         [HttpGet("{id}")]
-        public string Get(string id){
+        public string Get(string postKey,string id){
             return "value";
         }
         [HttpPost]
-        public void Post([FromBody]string value){
+        public Comment Post(string postKey, [FromBody]Comment comment){
+            var post = _dbContext.Posts.FirstOrDefault(x=>x.Key==postKey);
+            if(post ==null)return null;
+            comment.Post = post;
+            comment.Posted = DateTime.Now;
+            comment.Author = User.Identity.Name;
+            _dbContext.Comments.Add(comment);
+            _dbContext.SaveChanges();
+            return comment;
 
         }
         [HttpPut("{id}")]
